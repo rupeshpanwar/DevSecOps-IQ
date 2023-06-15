1b5ef1c33fb6d499655ccd973627c1220f6b5517
rupesh.panwar <rupeshpanwar28@gmail.com>
2023-06-15 15:16:31 +0800

    code change

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function toggleAnswer(sectionId) {
+function toggleSection(sectionId) {
+    const sections = document.getElementsByClassName('section');
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].style.display = 'none';
+    }
     const section = document.getElementById(sectionId);
-    section.classList.toggle('active');
-    const content = section.nextElementSibling;
+    section.style.display = 'block';
+  }
+  
+  function toggleAnswer(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.toggle('active');
+    const content = element.nextElementSibling;
     if (content.style.display === 'block') {
       content.style.display = 'none';
     } else {
