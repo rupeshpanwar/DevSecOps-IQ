833395c68842009748a5719b05cebc65c38e04bf
rupesh.panwar <rupeshpanwar28@gmail.com>
2023-08-06 11:46:33 +0800

    IQ

--- txt-2-html.py
+++ txt-2-html.py
@@ -1,7 +1,5 @@
-import os
-
 # Open and read the text file
-with open("raw/K8s-InterviewQs.txt", 'r') as f:
+with open("raw/k8s-qs.txt", 'r') as f:
     lines = f.readlines()
 
 # Prepare the HTML file content
@@ -24,11 +22,17 @@ html_content = """
 """
 
 # Add the Q&A sections to the HTML content
-for line in lines:
-    if line.startswith("Q:"):
-        html_content += f'<div class="qa-section"><div class="question"><h3>{line[3:].strip()}</h3></div>'
-    elif line.startswith("A:"):
-        html_content += f'<div class="answer"><p>{line[3:].strip()}</p></div></div>'
+for i in range(0, len(lines), 2):
+    question = lines[i].strip()
+    # Check if there is an answer line before trying to access it
+    if i + 1 < len(lines):
+        answer = lines[i+1].strip()
+        html_content += f'<div class="qa-section"><div class="question"><h3>{question}</h3></div>'
+        html_content += f'<div class="answer"><p>{answer}</p></div></div>'
+    else:
+        # If there is no answer, you can leave it blank or add a placeholder
+        html_content += f'<div class="qa-section"><div class="question"><h3>{question}</h3></div>'
+        html_content += f'<div class="answer"><p>No answer provided.</p></div></div>'
 
 # Close the HTML tags
 html_content += """
@@ -38,5 +42,5 @@ html_content += """
 """
 
 # Write the HTML content to a new HTML file
-with open("pages/K8s-InterviewQs.html", 'w') as f:
+with open("pages/K8s-Qs.html", 'w') as f:
     f.write(html_content)
